67158c47a74142445bf15d268276457c003cfbe2
RyanPham <roddyscodingservice@gmail.com>
2025-06-08 01:29:37 -0400

    implemented module 3 functionalities into calculator application

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,16 +1,24 @@
-def addition(a: float, b: float) -> float:
-    return a + b
+class Operations:
 
+    @staticmethod
+    def addition(a: float, b: float) -> float:
+        return a + b
+    
+    @staticmethod
+    def subtraction(a: float, b: float) -> float:
+        return a - b
+    
+    @staticmethod
+    def multiplication(a: float, b: float) -> float:
+        return a*b
+    
+    @staticmethod
+    def division(a: float, b: float) -> float:
+        #check if dividing by 0
+        if b == 0:
+            raise ValueError("Division by zero is not allowed.")
+        return a/b
 
-def subtraction(a: float, b: float) -> float:
-    return a - b
+        
+    
 
-
-def multiplication(a: float, b: float) -> float:
-    return a * b
-
-
-def division(a, b):
-    if b == 0:
-        raise ValueError("Division by zero is not allowed")
-    return a / b
